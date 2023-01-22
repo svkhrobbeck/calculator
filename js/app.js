db6470e2999f39ef62b9f06e526692d476a77631
suhrobfrontend <sukhrobfrontend@gmail.com>
2023-01-22 16:42:05 +0500

    update

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const elResultText = document.querySelector("[data-result-text]")
+const elButtons = document.querySelectorAll("[data-button]")
+const elOperators = document.querySelectorAll("[data-operator]")
+const elClearBtn = document.querySelectorAll("[data-clear]")
+const elCalcBtn = document.querySelector("[data-calc]")
+
+let firstOperand = ""
+let operator = null
+let secondOperand = ""
+
+elOperators.forEach(button => {
+  button.addEventListener("click", (e) => {
+    let elTarget = e.target.innerText
+    if (elResultText.textContent.includes(elTarget)) return
+    elResultText.textContent += elTarget
+    operator = elTarget
+  })
+})
+
+elButtons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    elTarget = e.target.innerText
+    if (elTarget !== "=") {
+      elResultText.textContent += elTarget
+    }
+    if (elResultText.textContent.includes(operator)) {
+      secondOperand += elTarget
+      console.log(secondOperand)
+    }
+    if (!elResultText.textContent.includes(operator)) {
+      firstOperand += elTarget
+      console.log(firstOperand)
+    }
+  })
+})
+
+elCalcBtn.addEventListener("click", () => {
+  let total = null
+  if (operator === "×") {
+    total = +firstOperand * +secondOperand
+  } else if (operator === "/") {
+    total = +firstOperand / +secondOperand
+  } else if (operator === "-") {
+    total = +firstOperand - +secondOperand
+  } else if (operator === "+") {
+    total = +firstOperand + +secondOperand
+  }
+  elResultText.textContent = total
+})
